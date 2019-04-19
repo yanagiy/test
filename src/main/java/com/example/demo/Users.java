b393188492456198ed2fdd9b189de03d58888070
tatsuya.yanagiya <tatsuya.yanagiya@g.softbank.co.jp>
2019-04-19 09:58:03 +0900

    04_18

---
+++ src/main/java/com/example/demo/Users.java
@@ -0,0 +1,52 @@
+/**
+ *
+ */
+package com.example.demo;
+
+public class Users {
+    private int id;
+    private int age;
+    private String name;
+    private String address;
+    private int tell;
+    private String mail;
+
+    public int getId() {
+        return id;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public int getTell() {
+        return tell;
+    }
+    public String getMail() {
+        return mail;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setTell(int tell) {
+        this.tell = tell;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+}
